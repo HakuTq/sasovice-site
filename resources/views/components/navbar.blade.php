100318846fe58a8353551638def7c2a657d34f8a
HakuTq <hakutq05@gmail.com>
2025-04-16 17:32:08 +0200

    started working on navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+    <div class="navbar-left">
+        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="navbar-logo">
+        <a href=""> Šašovice </a>
+    </div>
+    <div class="navbar-right">
+        <a href="" class="navbar-link">Home</a>
+        <a href="" class="navbar-link">About</a>
+        <a href="" class="navbar-link">Contact</a>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,31 +5,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title', config('app.name', 'Šašovice'))</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.scss', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body>
+        <div>
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+            @hasSection('header')
+                <header>
+                    <div>
+                        @yield('header')
                     </div>
                 </header>
-            @endisset
+            @endif
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                <x-navbar/>
+                @yield('content')
             </main>
         </div>
     </body>
