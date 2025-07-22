69d842281509f6e3b3dab5fc3bf8f8d0e4ee2c31
HakuTq <hakutq05@gmail.com>
2025-07-22 16:32:55 +0200

    Added responsivity to navbar and news/show

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,17 @@
 <div class="navbar">
     <div class="navbar-left">
-
-        <a href="{{ route('hunting.association') }}">{{__('Honební společenstvo Šašovice')}}</a>
+        <a href="{{ route('hunting.association') }}" class="navbar-title">
+            {{ __('Honební společenstvo Šašovice') }}
+        </a>
     </div>
-    <div class="navbar-right">
 
+    <!-- Hamburger button -->
+    <button class="hamburger" id="hamburger-btn" aria-label="Toggle menu">
+        ☰
+    </button>
+
+    <!-- Toggleable menu -->
+    <div class="navbar-right" id="navbar-menu">
         @if(request()->routeIs('contact'))
             <span class="navbar-link active">{{__('Kontakty')}}</span>
         @else
@@ -32,14 +39,17 @@
         <div class="language-switcher">
             <a href="{{ route('language.switch', 'cs') }}" 
                class="language-link {{ app()->getLocale() == 'cs' ? 'active' : '' }}"
-               title="@lang('czech')">
-                CZ
-            </a>
+               title="@lang('czech')">CZ</a>
             <a href="{{ route('language.switch', 'de') }}" 
                class="language-link {{ app()->getLocale() == 'de' ? 'active' : '' }}"
-               title="@lang('german')">
-                DE
-            </a>
+               title="@lang('german')">DE</a>
         </div>
     </div>
+
+<script>
+    document.getElementById('hamburger-btn').addEventListener('click', function () {
+        document.getElementById('navbar-menu').classList.toggle('active');
+    });
+</script>
+
 </div>
